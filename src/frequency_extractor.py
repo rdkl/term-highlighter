fdb0b2b61c1140b11d8a4043aa0ac3579c5dfb4e
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2015-03-20 01:11:34 +0300

    Add function to compute words frequencies

---
+++ src/frequency_extractor.py
@@ -0,0 +1,51 @@
+import xml.etree.ElementTree as ET
+import os
+from bs4 import BeautifulSoup
+import nltk
+
+def write_pure_text(string_list, filename):
+    path_to_data = "../data/fix_test_papers/"
+    f = open(path_to_data + filename + "_fix", "w")
+    for line in string_list:
+        print >>f, line.text.encode('utf8')
+    f.close()
+
+if __name__ == "__main__":
+    papers = []
+    num_papers = 10
+    path_to_data = "../data/test_papers/"
+    stemmer = nltk.stem.SnowballStemmer("english")
+    for (dirpath, dirnames, filenames) in os.walk(path_to_data):
+        papers.extend(filenames)
+        break
+    words_in_corpus = {}
+    words_in_papers = {}
+    
+    for i in xrange(num_papers):
+        print "Extract words from file", papers[i]
+        words_in_current_paper = set()
+        f = open(path_to_data + papers[i], "r")
+        soup = BeautifulSoup(f)
+        text = soup.find_all("p")
+        for string in text:
+            token_string = nltk.word_tokenize(string.text)
+            words_and_pos = nltk.pos_tag(token_string)
+            for w in words_and_pos:
+                stem_word = stemmer.stem(w[0])                    
+                if w[1] == "NN":
+                    if words_in_corpus.get(stem_word) is None:
+                        words_in_corpus[stem_word] = 0
+                    if words_in_papers.get(stem_word) is None:
+                        words_in_papers[stem_word] = 0
+                    words_in_corpus[stem_word] += 1
+                    if stem_word not in words_in_current_paper:
+                        words_in_papers[stem_word] += 1
+                        words_in_current_paper.add(stem_word)
+                                            
+        # write_pure_text(text, papers[i]) 
+        f.close()
+    print "Save words frequencies in the file..."
+    with open("../data/word_frequency", "w") as f:
+        for key in words_in_corpus:
+            print >>f, key.encode("utf8"), str(words_in_corpus[key]), str(words_in_papers[key])
+        print "Save words frequencies in the file... Done"
