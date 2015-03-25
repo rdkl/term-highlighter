614376daaab8003a59cadaa0e76d475ad537861c
Sergey Voronov <rdkl.hrd@gmail.com>
2015-03-26 02:28:36 +0300

    25% speed-up of find_terms

--- src/python/find_terms.py
+++ src/python/find_terms.py
@@ -2,11 +2,13 @@ from multiprocessing import Pool
 import wikipedia
 import time 
 
+import gevent.threadpool as tp
 
 #-----------------------------------------------------------------------------
 def get_expected_pages_names(args):
     (term, number_of_results) = args
-    return (term, wikipedia.search(term, number_of_results))
+    result = (term, wikipedia.search(term, number_of_results))
+    return result
 
 #-----------------------------------------------------------------------------
 def get_one_summary(args):
@@ -18,23 +20,29 @@ def get_one_summary(args):
     return (summary, term)
 
 #-----------------------------------------------------------------------------
-def find_terms(terms, number_of_results, get_best_summary):
+def find_terms(terms, number_of_results, get_best_summary, show_time = False):
     max_sentences = 10
+    if show_time:
+        start_time = time.time()
     
     hints = {}
     
-    p = Pool(20)
+    #p = Pool(8)
+    p = tp.ThreadPool(8)
     tasks = ((term, number_of_results) for term in terms)
     search_results = dict(p.map(get_expected_pages_names, tasks))
+    if show_time:
+        print "Time: %.2f" % (time.time() - start_time)
     
     donwload_tasks = ((term, article, max_sentences) 
                       for term in search_results
                       for article in search_results[term])
     
     summaries_and_terms = p.map(get_one_summary, donwload_tasks)
+    if show_time:
+        print "Time: %.2f" % (time.time() - start_time)
     
     summaries = {}
-    
     for summary, term in summaries_and_terms:
         if len(summary) == 0:
             continue
@@ -43,10 +51,12 @@ def find_terms(terms, number_of_results, get_best_summary):
             summaries[term] += [summary]
         else:
             summaries[term] = [summary]
-            
+    
+    if show_time:
+        print "Time: %.2f" % (time.time() - start_time)
+    
     for term in summaries:
         hints[term] = get_best_summary(summaries[term])
-
     return hints
     
 #-----------------------------------------------------------------------------
@@ -56,7 +66,8 @@ if __name__ == "__main__":
     def get_summary(summaries):
         return summaries[0]
     
-    hints = find_terms(["neuron", "serine", "lysis"], 4, get_summary)
+    hints = find_terms(["neuron", "serine", "lysis", "mitosis", 
+                        "redox", "cyst", "ligase"], 5, get_summary)
     for term in hints:
         print "-" * 80
         print term
