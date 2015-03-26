273871b4f1014fed5d6bde085674c7db4aa03aac
Maria Ryskina <m.ryskina@gmail.com>
2015-03-26 04:16:24 +0300

    Bugs fixed

--- src/euclidean_dist.py
+++ src/euclidean_dist.py
@@ -1,10 +1,10 @@
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.porter import PorterStemmer
 import re
 import collections
 
 def euclidean_dist(input_text, ref_text, allowed_dict, banned_dict):
 
-    st = LancasterStemmer()
+    st = PorterStemmer()
     
     input_text_list = re.split(', |\. |; | |\n', input_text)
     ref_text_list = re.split(', |\. |; | |\n', ref_text)
@@ -17,30 +17,24 @@ def euclidean_dist(input_text, ref_text, allowed_dict, banned_dict):
     s = 0  
     input_vector = []
     ref_vector = []
-    inp_norm = 0
-    ref_norm = 0
     
     if bool(allowed_dict):
         allowed_words = allowed_dict.keys()
         for word in allowed_words:
             input_vector.append(input_counter[word])
             ref_vector.append(ref_counter[word])
-            inp_norm += input_counter[word]
-            ref_norm += ref_counter[word]
     else:
-        input_words = input_counter.keys() 
+        input_words = dict(input_counter.items()+ref_counter.items()).keys() 
         for word in input_words:
             if word not in banned_dict:
                 input_vector.append(input_counter[word])
                 ref_vector.append(ref_counter[word])
-                inp_norm += input_counter[word]
-                ref_norm += ref_counter[word]
                         
-    inp_norm = max(inp_norm, 1)
-    ref_norm = max(ref_norm, 1)
-    
+    inp_norm = max(sum(input_vector), 1)
+    ref_norm = max(sum(ref_vector), 1)
+
     for i in range(len(input_vector)):
-        s += (input_vector[i]/float(inp_norm) - ref_vector[i]/float(ref_norm))**2
+        s += (input_vector[i] - ref_vector[i])**2
     dist = s**.5
         
     return dist
@@ -48,8 +42,8 @@ def euclidean_dist(input_text, ref_text, allowed_dict, banned_dict):
 #-----------------------------------------------------------------------------
 if __name__ == "__main__":
     input_text = 'fat cats, cats; cats were sitting on the mat'
-    ref_text = 'fat bat'
-    allowed_dict = {'fat':1, 'cat':2, 'bat':3, 'rat':6}
+    ref_text = ''
+    allowed_dict = {'fat':1, 'cat':2}
     banned_dict = {}    
     dist = euclidean_dist(input_text, ref_text, allowed_dict, banned_dict)
     print dist
--- src/hellinger_dist.py
+++ src/hellinger_dist.py
@@ -1,15 +1,15 @@
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.porter import PorterStemmer
 import re
 import collections
 
 def hellinger_dist(input_text, ref_text, allowed_dict, banned_dict):
 
-    st = LancasterStemmer()
+    st = PorterStemmer()
     
     input_text_list = re.split(', |\. |; | |\n', input_text)
     ref_text_list = re.split(', |\. |; | |\n', ref_text)
     input_text_list  = [st.stem(word) for word in input_text_list]
-    ref_text_list  = [st.stem(word) for word in ref_text_list]  
+    ref_text_list  = [st.stem(word) for word in ref_text_list]
     
     input_counter = collections.Counter(input_text_list)
     ref_counter = collections.Counter(ref_text_list)
@@ -17,39 +17,33 @@ def hellinger_dist(input_text, ref_text, allowed_dict, banned_dict):
     s = 0  
     input_vector = []
     ref_vector = []
-    inp_norm = 0
-    ref_norm = 0
     
     if bool(allowed_dict):
         allowed_words = allowed_dict.keys()
         for word in allowed_words:
             input_vector.append(input_counter[word])
             ref_vector.append(ref_counter[word])
-            inp_norm += input_counter[word]
-            ref_norm += ref_counter[word]
     else:
-        input_words = input_counter.keys() 
+        input_words = dict(input_counter.items()+ref_counter.items()).keys()
         for word in input_words:
             if word not in banned_dict:
                 input_vector.append(input_counter[word])
                 ref_vector.append(ref_counter[word])
-                inp_norm += input_counter[word]
-                ref_norm += ref_counter[word]
                         
-    inp_norm = max(inp_norm, 1)
-    ref_norm = max(ref_norm, 1)
-    
-    for i in range(len(input_vector)):
-        s += ((input_vector[i]/float(inp_norm))**.5 - (ref_vector[i]/float(ref_norm))**.5)**2
-    dist = (0.5*s)**.5
+    if (sum(input_vector) == 0 or sum(ref_vector) == 0):
+        dist = 1.0
+    else:    
+        for i in range(len(input_vector)):
+            s += ((input_vector[i]/float(sum(input_vector)))**.5 - (ref_vector[i]/float(sum(ref_vector)))**.5)**2
+        dist = (0.5*s)**.5
         
     return dist
 
 #-----------------------------------------------------------------------------
 if __name__ == "__main__":
     input_text = 'fat cats, cats; cats were sitting on the mat'
-    ref_text = 'fat bat'
-    allowed_dict = {'fat':1, 'cat':2, 'bat':3, 'rat':6}
+    ref_text = ''
+    allowed_dict = {'fat':5}
     banned_dict = {}    
     dist = hellinger_dist(input_text, ref_text, allowed_dict, banned_dict)
     print dist
